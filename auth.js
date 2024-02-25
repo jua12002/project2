0457066329e82deffa240b6d7a9d74bbcb3fd059
jua12002 <jua12002@byui.edu>
2024-02-24 22:28:39 -0600

    workingwithAuth

---
+++ auth.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+const GOOGLE_CLIENT_ID = '731739764635-ob0gqfhkaulf37ehvftovud6kt08nj64.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:8085/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    return done(err, user);
+    ////return done(err, profile);
+    
+    /*User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });*/
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
@@ -12,6 +13,68 @@ app
     next();
   })
   .use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on port ${port}`);
+  }
+});
+*/
+
+const express = require('express');
+const session = require("express-session");
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+require('./auth');
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+const port = process.env.port || 8085;
+const app = express();
+app.use(session({ secret: 'cats' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+    next();
+  })
+  .use('/', require('./routes'));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:  ['email', 'profile'] })
+);
+
+app.get('/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/auth/failure', (req, res) => {
+  res.send('Something went wrong..');
+});
+  
+app.get('/protected', isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get('/logout', (req, res) => {
+  req.logOut();
+  req.session.destroy();
+  res.send('Goodbye!');
+})
 
 mongodb.initDb((err) => {
   if (err) {
