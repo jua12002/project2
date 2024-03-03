816902ccbdb0a425114cc8970b0a0f2f2422fdd4
jua12002 <jua12002@byui.edu>
2024-03-02 22:12:39 -0600

    Saturday

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,14 +3,19 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+//Get all contacts
 router.get('/', contactsController.getAll);
 
+//Get one contact by id
 router.get('/:id', contactsController.getSingle);
 
+//POST - Create new contact
 router.post('/', contactsController.createContact);
 
+//PUT or Update contact by Id
 router.put('/:id', contactsController.updateContact);
 
+//DELETE one contact by Id
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
